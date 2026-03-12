9addf68afd61f0a8efac2be5077db91be35743bf
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-12 16:12:46 +0530

    Day02_Variables_and_Data_Types

---
+++ Day02_Variables_and_Data_Types/example.py
@@ -0,0 +1,31 @@
+# Variables
+name = "xyz"
+age = 16
+score = 89.4
+is_student = True
+
+print("\nPrinting the values in the variables: ")
+print("Name: ", name)
+print("Age: ", age)
+print("Score: ", score)
+print("Is a student? : ", is_student)
+
+# printing the data types
+print("\nPrinting the data types: ")
+print("Name: ", type(name))
+print("Age: ", type(age))
+print("Score: ", type(score))
+print("Is a student? : ", type(is_student))
+
+# Multiple variable assignment
+x, y, z = 10, 20, 30
+print("\nPrinting multiple variable assignment: ")
+print(x, y, z)
+
+# Dynamic typing
+value = 10
+print("\nDynamic printing of variable value: ")
+print(value)
+
+value = "ten"
+print(value)
