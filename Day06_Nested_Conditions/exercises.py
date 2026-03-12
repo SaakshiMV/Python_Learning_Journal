28ba398b912fb4c7100dfd48be2b38b982b866bd
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-13 00:02:55 +0530

    Day06_Nested_Conditional
    Day 6: Nested Conditional Statements
    
    - Learned nested if statements
    - Practiced layered decision logic
    - Built simple login and eligibility checks
    - Completed exercises using nested conditions

---
+++ Day06_Nested_Conditions/exercises.py
@@ -0,0 +1,57 @@
+# Exercise 1
+# Check student eligibility for exam
+print("Check student eligibility for exam: ")
+attendance = 80
+fees_paid = True
+
+if attendance >= 75:
+    if fees_paid:
+        print("Eligible for exam")
+    else:
+        print("Fees not paid")
+else:
+    print("Attendance too low")
+
+#=======================================================
+# Exercise 2
+# ATM withdrawal simulation
+print("ATM withdrawal simulation: ")
+balance = 1000
+withdraw = 500
+
+if withdraw <= balance:
+    print("Withdrawal successful")
+else:
+    print("Insufficient balance")
+
+
+#=======================================================
+# Exercise 3
+# Login system
+print("Login system: ")
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin":
+    if password == "python123":
+        print("Login successful")
+    else:
+        print("Wrong password")
+else:
+    print("Invalid username")
+
+
+#=======================================================
+# Exercise 4
+# Age and citizenship check
+print("Age and citizenship check: ")
+age = int(input("Enter your age: "))
+citizen = True
+
+if age >= 18:
+    if citizen:
+        print("Eligible to vote")
+    else:
+        print("Not a citizen")
+else:
+    print("Underage")
