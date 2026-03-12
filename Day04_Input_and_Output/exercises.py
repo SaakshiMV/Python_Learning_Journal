5d17e8579760e439d9c8e85ec79c135f80e73daf
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-12 18:35:22 +0530

    Day04_Input_and_Output
    Day 4: Input and Output in Python
    
    - Learned how to use print()
    - Practiced taking user input with input()
    - Converted input types using int() and float()
    - Implemented simple interactive programs

---
+++ Day04_Input_and_Output/exercises.py
@@ -0,0 +1,25 @@
+# Exercise 1
+# Ask the user for their name and print a greeting
+name = input("Enter your name: ")
+print("Hello", name)
+
+#===============================================================
+# Exercise 2
+# Ask the user for their birth year and calculate age
+birth_year = int(input("Enter your birth year: "))
+age = 2026 - birth_year
+print("Your age is:", age)
+
+#===============================================================
+# Exercise 3
+# Create a simple addition program
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Result:", a + b)
+
+#================================================================
+# Exercise 4
+# Ask user for favorite programming language
+language = input("Enter your favorite programming language: ")
+print(f"{language} is a great choice!")
