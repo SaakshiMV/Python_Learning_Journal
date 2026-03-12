f5fdbcf8ee25c56859ffddd6dd147d6f8a58c59e
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-12 18:07:21 +0530

    Day03_Operators_in_Python
    Day 3: Python Operators
    
    - Learned arithmetic operators
    - Practiced comparison operators
    - Explored logical operators
    - Implemented assignment operators
    - Completed practice exercises

---
+++ Day03_Operators_in_Python/exercises.py
@@ -0,0 +1,26 @@
+# Exercise 1
+# Calculate area of rectangle
+length = 10
+width = 5
+
+area = length * width
+print("\nArea of rectangle: ", area)
+
+#====================================================
+# Exercise 2
+# Check if a number is greater than 50
+num = 75
+print("\nGreater no: ", num > 50)
+
+#====================================================
+# Exercise 3
+# Logical operator practice
+age = 22
+citizen = True
+print("\nEligible for voting: ", age >= 18 and citizen)
+
+#====================================================
+# Exercise 4
+# Modulus example
+number = 17
+print("\nModulus: ", number % 2)
