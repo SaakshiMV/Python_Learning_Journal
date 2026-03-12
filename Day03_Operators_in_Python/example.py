63c4ad91555eec11979445b91528954ac39dc933
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-12 18:03:48 +0530

    Day03_Operators_in_Python
    Day 3: Python Operators
    
    - Learned arithmetic operators
    - Practiced comparison operators
    - Explored logical operators
    - Implemented assignment operators
    - Completed practice exercises

---
+++ Day03_Operators_in_Python/example.py
@@ -0,0 +1,34 @@
+# Arithmetic operators
+a = 10
+b = 3
+print("\nArithmetic operators: ")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+
+# Comparison operators
+x = 10
+y = 5
+print("\nComparision operators: ")
+print(x > y)
+print(x == y)
+print(x != y)
+
+# Logical operators
+age = 20
+has_id = True
+print("\nLogical operators: ")
+print(age > 18 and has_id)
+
+# Assignment operators
+num = 10
+num += 5       # same as num = num + 5
+print("\nAssignment operators: ")
+print(num)
+
+#Operator Precedence
+print("\nOperator Precedence: ")
+result = 10 + 5 * 2
+print(result)
