c78f1059281963daeb838798d8e61adbf43349c5
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-13 18:40:19 +0530

    Pattern_Printing

---
+++ Projects/Pattern_Printing/pattern05.py
@@ -0,0 +1,24 @@
+# pattern 5
+#         * 
+#       * * * 
+#     * * * * * 
+#   * * * * * * * 
+#     * * * * * 
+#       * * * 
+#         * 
+
+rows = int(input("Enter the rows : "))
+
+for i in range(1, rows + 1):
+    for j in range(rows - i):
+        print(" ", end = " ")
+    for k in range(2*i - 1):
+        print("*", end = " ")
+    print()
+
+for i in range(rows - 1, 0, -1):
+    for j in range(rows - i):
+        print(" ", end = " ")
+    for k in range(2*i - 1):
+        print("*", end = " ")
+    print()
