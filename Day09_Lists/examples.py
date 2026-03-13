d0fc8c4be9e75a2291aace87dd5f2636a89a5f5e
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-13 12:40:35 +0530

    Day09_Lists
    Day 9: Lists in Python
    
    - Learned how to create and use lists
    - Practiced accessing and modifying list elements
    - Used append() and remove() methods
    - Iterated through lists using loops

---
+++ Day09_Lists/examples.py
@@ -0,0 +1,28 @@
+# Creating lists
+fruits = ["apple", "banana", "mango"]
+numbers = [10, 20, 30]
+
+
+# Accessing elements
+print(fruits[0])
+print(fruits[1])
+
+
+# Modifying list
+fruits[1] = "orange"
+print(fruits)
+
+
+# Adding items
+numbers.append(40)
+print(numbers)
+
+
+# Removing items
+fruits.remove("apple")
+print(fruits)
+
+
+# Looping through list
+for fruit in fruits:
+    print(fruit)
