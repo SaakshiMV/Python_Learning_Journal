409d16e85caa9d52b11f28efe681888de49ec4b9
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-13 12:42:23 +0530

    Day09_Lists
    Day 9: Lists in Python
    
    - Learned how to create and use lists
    - Practiced accessing and modifying list elements
    - Used append() and remove() methods
    - Iterated through lists using loops

---
+++ Day09_Lists/exercises.py
@@ -0,0 +1,29 @@
+# Exercise 1
+# Create a list of 5 numbers and print them
+print("\nExample 1: ")
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+    print(num)
+
+
+# Exercise 2
+# Create a list of favorite movies
+print("\nExample 2: ")
+movies = ["Inception", "Interstellar", "Avatar"]
+print(movies)
+
+
+# Exercise 3
+# Add an item to a list
+print("\nExample 3: ")
+colors = ["red", "blue", "green"]
+colors.append("yellow")
+print(colors)
+
+
+# Exercise 4
+# Remove an item from a list
+print("\nExample 4: ")
+animals = ["dog", "cat", "lion"]
+animals.remove("cat")
+print(animals)
