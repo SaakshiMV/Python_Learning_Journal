f86e9d27194e7d608298f2770878f5db44fb659e
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-13 12:04:34 +0530

    Day07_Loops
    Day 7: Loops in Python
    
    - Learned for loops and while loops
    - Used range() for iteration
    - Practiced repeating tasks with loops
    - Completed exercises including sums and countdown

---
+++ Day07_Loop/examples.py
@@ -0,0 +1,27 @@
+# Example 1: for loop
+print("\n=====================================")
+for i in range(5):
+    print("Hello Python")
+
+
+# Example 2: print numbers 1–10
+print("\n=====================================")
+for i in range(1, 11):
+    print(i)
+
+
+# Example 3: while loop
+print("\n=====================================")
+count = 1
+while count <= 5:
+    print("Count:", count)
+    count += 1
+
+
+# Example 4: simple sum
+print("\n=====================================")
+total = 0
+for i in range(1, 6):
+    total += i
+
+print("Sum:", total)
