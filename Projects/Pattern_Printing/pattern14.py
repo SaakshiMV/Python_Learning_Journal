42f56305b47ae29a21c2c9c45870699d60125002
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-18 16:04:18 +0530

    Pattern_Printing

---
+++ Projects/Pattern_Printing/pattern14.py
@@ -0,0 +1,16 @@
+#pattern 14
+#         1 
+#       1 2 1 
+#     1 2 3 2 1 
+#   1 2 3 4 3 2 1 
+# 1 2 3 4 5 4 3 2 1 
+
+rows = int(input("Enter the row size for the pattern: "))
+for i in range(1, rows + 1):
+    for j in range(rows  - i):
+        print(" ", end=" ")
+    for k in range(1, i + 1):
+        print(k, end=" ")
+    for l in range(i - 1, 0, -1):
+        print(l, end=" ")
+    print()
