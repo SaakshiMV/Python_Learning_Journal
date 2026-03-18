1eb2d6daa86d63a16f38814e86def9a9f41c304a
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-18 13:16:08 +0530

    Pattern_Printing

--- Projects/Pattern_Printing/pattern08.py
+++ Projects/Pattern_Printing/pattern08.py
@@ -6,8 +6,8 @@
 # * 
 
 rows = int(input("Enter the row size for the pattern: "))
-for i in range(rows, 0, -1):
-    for j in range(rows, 0, -1):
+for i in range(rows , 0, -1):
+    for j in range(1, i + 1):
         if j == 1 or i == rows or i == j:
             print("*", end=" ")
         else:
