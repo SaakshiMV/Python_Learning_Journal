901b26b23c8536ac2b9abeface2ef928d8f06fcb
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-18 16:31:18 +0530

    Pattern_Printing

---
+++ Projects/Pattern_Printing/pattern20.py
@@ -0,0 +1,16 @@
+#pattern 20
+# A B C D E D C B A 
+#   A B C D C B A 
+#     A B C B A 
+#       A B A 
+#         A 
+
+rows = int(input("Enter the row size for the pattern: "))
+for i in range(rows, 0, -1):
+    for j in range(rows - i):
+        print(" ", end=" ")
+    for k in range(1, i + 1):
+        print(chr(64 + k), end=" ")
+    for l in range(i - 1, 0, -1):
+        print(chr(64 + l), end=" ")
+    print()
