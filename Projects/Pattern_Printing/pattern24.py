ac61558df30aa58e961a18900f19ccbc135cbdf0
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-19 19:00:03 +0530

    Pattern_Printing

---
+++ Projects/Pattern_Printing/pattern24.py
@@ -0,0 +1,29 @@
+# pattern 24
+# *                 * 
+# * *             * * 
+# * * *         * * * 
+# * * * *     * * * * 
+# * * * * * * * * * * 
+# * * * * * * * * * * 
+# * * * *     * * * * 
+# * * *         * * * 
+# * *             * * 
+# *                 * 
+
+rows = int(input("Enter the row size for the pattern: "))
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    for j in range(2 * (rows - i)):
+        print(" ", end=" ")
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+for i in range(rows, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    for j in range(2 * (rows - i)):
+        print(" ", end=" ")
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
