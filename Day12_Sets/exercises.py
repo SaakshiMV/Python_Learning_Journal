7923150439c9957f98d27abd716d2d93bec2dd28
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-19 21:57:10 +0530

    Day12_Sets
    Day 12: Sets in Python
    
    - Learned set creation and properties
    - Practiced adding and removing elements
    - Used set operations (union, intersection, difference)
    - Implemented duplicate removal example

---
+++ Day12_Sets/exercises.py
@@ -0,0 +1,23 @@
+# Exercise 1
+# Create a set and print it
+numbers = {10, 20, 30}
+print(numbers)
+
+# Exercise 2
+# Remove duplicates from a list
+data = [1, 2, 2, 3, 3, 4]
+unique_data = set(data)
+print(unique_data)
+
+# Exercise 3
+# Add and remove elements
+colors = {"red", "blue"}
+colors.add("green")
+colors.remove("red")
+print(colors)
+
+# Exercise 4
+# Find common elements between two sets
+set1 = {1, 2, 3}
+set2 = {2, 3, 4}
+print(set1 & set2)
