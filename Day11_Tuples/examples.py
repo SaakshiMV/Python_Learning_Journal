374d256351ef19a9d170aba0b48d852a2132a6a4
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-19 21:47:07 +0530

    Day11_Tuples

---
+++ Day11_Tuples/examples.py
@@ -0,0 +1,18 @@
+# Creating tuples
+numbers = (1, 2, 3, 4)
+fruits = ("apple", "banana", "mango")
+
+# Accessing elements
+print(fruits[0])
+print(fruits[1])
+
+# Length of tuple
+print(len(numbers))
+
+# Looping through tuple
+for fruit in fruits:
+    print(fruit)
+
+# Tuple with mixed data types
+data = ("xyz", 30, True)
+print(data)
