f1df966a8c1b9e2b3c0a6a2563a1757a9b0c9080
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-20 11:42:36 +0530

    Day16_Functions
    Day 16: Functions in Python
    
    - Learned function definition and usage
    - Practiced parameters and return values
    - Built reusable utility functions
    - Improved code structure and modularity

---
+++ Day16_Functions/exercises.py
@@ -0,0 +1,27 @@
+# Exercise 1
+# Create a function to print your name
+def my_name():
+    print("xyz")
+my_name()
+
+# Exercise 2
+# Function to add two numbers
+def add(a, b):
+    return a + b
+print(add(3, 4))
+
+# Exercise 3
+# Function to check even or odd
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+print(check_even_odd(10))
+
+# Exercise 4
+# Function with default parameter
+def greet(name="Guest"):
+    print(f"Hello, {name}")
+greet()
+greet("John")
