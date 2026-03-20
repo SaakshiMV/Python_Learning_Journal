0946ee0b6153c9f9c2a5886db1cb9b207958987e
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-20 11:30:37 +0530

    Day14_Strings
    Day 14: Strings in Python
    
    - Learned string operations and methods
    - Practiced slicing and text manipulation
    - Built a text analyzer mini project

---
+++ Day14_Strings/exercises.py
@@ -0,0 +1,20 @@
+# Exercise 1
+# Print first and last character
+text = "Python"
+print(text[0])
+print(text[-1])
+
+# Exercise 2
+# Convert string to uppercase
+name = "xyz"
+print(name.upper())
+
+# Exercise 3
+# Replace a word
+sentence = "I love Python"
+print(sentence.replace("Python", "coding"))
+
+# Exercise 4
+# Check if string contains only digits
+num = "12345"
+print(num.isdigit())
