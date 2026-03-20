86b1456549fd8d7ba7d0a0bd1b9f3af2cc784e13
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-20 11:29:37 +0530

    Day14_Strings
    Day 14: Strings in Python
    
    - Learned string operations and methods
    - Practiced slicing and text manipulation
    - Built a text analyzer mini project

---
+++ Day14_Strings/examples.py
@@ -0,0 +1,22 @@
+# Creating strings
+text = "Python Programming"
+
+# Accessing characters
+print(text[0])
+print(text[7])
+
+# Slicing
+print(text[0:6])
+print(text[7:])
+
+# String methods
+print(text.upper())
+print(text.lower())
+print(text.replace("Python", "Java"))
+
+# Checking
+print(text.isalpha())
+print(text.isalnum())
+
+# Length
+print(len(text))
