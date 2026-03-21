89e87f437913df805d43644374c41bf602babf52
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-21 15:01:59 +0530

    Day18_File_Handling
    Day 18: File Handling in Python
    
    - Learned file operations (read, write, append)
    - Used 'with' for safe file handling
    - Built a simple notes app using file storage

--- Day18_File_Handling/exercises.py
+++ Day18_File_Handling/exercises.py
@@ -18,3 +18,33 @@ with open("name.txt", "a") as file:
 with open("name.txt", "r") as file:
     content = file.read()
     print(len(content))
+
+# Exercises 5
+# Simple Notes App
+while True:
+    print("\n1. Add Note")
+    print("2. View Notes")
+    print("3. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        note = input("Enter your note: ")
+        with open("notes.txt", "a") as file:
+            file.write(note + "\n")
+        print("Note saved!")
+
+    elif choice == "2":
+        try:
+            with open("notes.txt", "r") as file:
+                print("\nYour Notes:")
+                print(file.read())
+        except FileNotFoundError:
+            print("No notes found.")
+
+    elif choice == "3":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice!")
