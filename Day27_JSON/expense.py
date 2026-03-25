044485b01e90cafea611cb09cf7a17ca15ae8403
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-25 23:18:28 +0530

    Day27_JSON

---
+++ Day27_JSON/expense.py
@@ -0,0 +1,10 @@
+class Expense:
+    def __init__(self, title, amount):
+        self.title = title
+        self.amount = amount
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "amount": self.amount
+        }
