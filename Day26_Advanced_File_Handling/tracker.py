0312e15088e6115b9bbeafa7cf3b6628d5857137
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-25 11:42:31 +0530

    Day26_Advanced_File_Handling

---
+++ Day26_Advanced_File_Handling/tracker.py
@@ -0,0 +1,41 @@
+import json
+from expense import Expense
+
+class ExpenseTracker:
+    def __init__(self, file_name="data.json"):
+        self.file_name = file_name
+
+    def load_data(self):
+        try:
+            with open(self.file_name, "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return []
+        except json.JSONDecodeError:
+            return []
+
+    def save_data(self, data):
+        with open(self.file_name, "w") as file:
+            json.dump(data, file, indent=4)
+
+    def add_expense(self, title, amount):
+        data = self.load_data()
+        expense = Expense(title, amount)
+        data.append(expense.to_dict())
+        self.save_data(data)
+        print("Expense added!")
+
+    def view_expenses(self):
+        data = self.load_data()
+        if not data:
+            print("No expenses found.")
+            return
+
+        print("\nExpenses:")
+        for exp in data:
+            print(f"{exp['title']} - ₹{exp['amount']}")
+
+    def total_expense(self):
+        data = self.load_data()
+        total = sum(exp["amount"] for exp in data)
+        print(f"\nTotal Expense: ₹{total}")
