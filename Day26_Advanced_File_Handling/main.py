ec3d4847423deb9dcb4e5daa7c478d46970f314f
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-25 11:42:55 +0530

    Day26_Advanced_File_Handling

---
+++ Day26_Advanced_File_Handling/main.py
@@ -0,0 +1,32 @@
+from tracker import ExpenseTracker
+
+tracker = ExpenseTracker()
+
+while True:
+    print("\n1. Add Expense")
+    print("2. View Expenses")
+    print("3. Total Expense")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        title = input("Enter title: ")
+        try:
+            amount = float(input("Enter amount: "))
+            tracker.add_expense(title, amount)
+        except ValueError:
+            print("Invalid amount!")
+
+    elif choice == "2":
+        tracker.view_expenses()
+
+    elif choice == "3":
+        tracker.total_expense()
+
+    elif choice == "4":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice!")
