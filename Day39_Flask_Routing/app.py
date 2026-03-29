09bc6f182edd32a3be6eea24ce6caed8b0eb0268
SaakshiMV <saakshimvernekar29@gmail.com>
2026-03-29 20:49:49 +0530

    Day39_Flask_Routing

---
+++ Day39_Flask_Routing/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    name = "Saakshi"
+    return render_template("index.html", name=name)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
